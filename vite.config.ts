47cad004d4454bf8c0b7a25385c9de4a960d038e
abiygetahun2005-ctrl <abiygetahun2005@gmail.com>
2026-04-02 10:48:18 +0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/FUTURE_FS_01/", 
-
+  base: "/FUTURE_FS_01/",
   server: {
     host: "::",
     port: 8080,
@@ -16,9 +16,7 @@ export default defineConfig(({ mode }) => ({
       "/api": "http://localhost:5000",
     },
   },
-
-  plugins: [react()], // 👈 removed lovable
-
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
